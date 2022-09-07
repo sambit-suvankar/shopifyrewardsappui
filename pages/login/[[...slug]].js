ef7d24a223d41ada2b8b364c5d5346fc96fa4d40
Girish <gpatil@saadiadirect.com>
2022-09-07 19:10:59 +0530

    removed multipass

--- pages/login/[[...slug]].js
+++ pages/login/[[...slug]].js
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Link from 'next/link';
-import Multipassify from 'multipassify';
 import {useRouter} from "next/router"
 import { fetchPaymentRequest } from "../../store/actions/adsActions";
 
@@ -13,7 +12,6 @@ function Login({
   paymentReq,
   loading,
   cards,
-  ip,
   ...props
 }) {
   const { register, formState: { errors }, handleSubmit } = useForm({criteriaMode: "all"});
@@ -22,26 +20,7 @@ function Login({
   const onSubmit = (data) => {
     console.log("login "+data);    
   }
-  const getIPData = async () => {
-    const res = await fetch('https://geolocation-db.com/json/');
-    const json = await res.json();
-    console.log(json);
-  }
-  const handleOnClick = () =>{
-    console.log('Login wirh shopify '+paymentReq.customer.email,ip);
-
-    // Construct the Multipassify encoder
-    var multipassify = new Multipassify("b740a1d5ea31cb36cb7d7fc0e8291f5c");
-    // Create your customer data hash
-    var customerData = { email: paymentReq.customer.email, remote_ip:ip, return_to:"https://shopifyrewardsappui.vercel.app/payment"};
-
-    // Encode a Multipass token
-    var token = multipassify.encode(customerData);
-    console.log('token',token)
-    // Generate a Shopify multipass URL to your shop
-    var url = multipassify.generateUrl(customerData, "dev1-lord-and-taylor.myshopify.com");
-    window.location.href = url;
-  }
+ 
   const { slug } = useRouter().query;
   const id = slug && slug.length > 0 && slug[0];
   useEffect (() => {
@@ -109,7 +88,6 @@ function Login({
       paymentReq && 
       <div className="payment_details">
         <Link href={paymentReq.cancel_url} >Cancel and go back to store</Link>
-        <button onClick={() => handleOnClick()}>Login with Shopify</button>
       </div>
     }
     
@@ -130,19 +108,7 @@ function mapStateToProps(state, ownProps) {
 
   return {cards:json}
 } */
-export async function getServerSideProps({req}) {
-  let ip;
-  if (req.headers["x-forwarded-for"]) {
-    ip = req.headers["x-forwarded-for"].split(',')[0]
-  } else if (req.headers["x-real-ip"]) {
-    ip = req.connection.remoteAddress
-  } else {
-    ip = req.connection.remoteAddress
-  }
-  return {
-    props: {ip}, // will be passed to the page component as props
-  }
-}
+
 const mapDispatchToProps = {
   fetchPaymentRequest 
 };
