08f9c20035754f8f77911f500b89d1b40f47567e
unknown <SAMBIT@SGINLTW11SAMBI.NYCO.GROUP.COM>
2022-09-21 14:16:18 +0530

    Added error handling and balance section of modal

--- pages/rewardCardDetails.js
+++ pages/rewardCardDetails.js
@@ -1,16 +1,17 @@
 import React, {useState, useRef, useEffect} from "react";
 import { connect } from "react-redux";
 import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 import { removeRcNumber } from "../store/actions/apiStatusActions";
 import { IoMdArrowDropright, IoMdArrowDropdown } from "react-icons/io";
 
-function RewardCardDetailsContainer ({ rcInputRef ,rcDetails, removeRcNumber, onRcNumberSubmit, toggleModal, loadingSubmit}){
+function RewardCardDetailsContainer ({ rcDetails, rcCard, removeRcNumber, onRcNumberSubmit, toggleModal, loadingSubmit, appliedMsg, costToCredit}){
     const [ collapsible, setCollapsible] = useState(false);
     const rewardCerf = useRef()
     const {
-        register : register2,
-        formState: { errors : errors2 },
-        handleSubmit : handleSubmit2,
+        register : register3,
+        formState: { errors : errors3 },
+        handleSubmit : handleSubmit3,
         reset 
       } = useForm({ criteriaMode: "all" });
     
@@ -41,36 +42,64 @@ function RewardCardDetailsContainer ({ rcInputRef ,rcDetails, removeRcNumber, on
 
                 <div className="reward-cards">
                   {rcDetails.length > 0 ? 
-                  rcDetails.map((e,i) => (<div className="rc-card" key={i}><span>CERTIFICATE {e.rcNumber.replace(/\d{12}(\d{4})/, "************$1")}</span> <button id={e.rcNumber} onClick={(e)=> deleteRcNumber(e) }>REMOVE</button></div>)) : 
+                  rcDetails.map((e,i) => (<div className="rc-card" key={i}><span>CERTIFICATE {e.cardNumber.replace(/\d{15}(\d{4})/, "***** ***** ***** $1")}</span> <button id={e.cardNumber} onClick={(e)=> deleteRcNumber(e) }>REMOVE</button></div>)) : 
                   <></>
                   }
                 </div>
                 
                 <div className="rewards-certificate hide-reward" ref={rewardCerf}>
-                    <form style={{ width: "100%", margin: "0"}} onSubmit={handleSubmit2(onRcNumberSubmit)}>
+                    <form style={{ width: "100%", margin: "0"}} onSubmit={handleSubmit3(onRcNumberSubmit)}>
                       <div style={{display:"flex", justifyContent:"space-between"}}>
                         <div style={{width: "75%", marginRight: "10px"}}>
                           <label htmlFor="rcnumber">Reward Certificate Number</label>
-                          <input ref={rcInputRef} id="rcnumber" {...register2("rcnumber",{minLength: {
-                        value: 16,
-                        message: "Card number must exceed 15 digits",
-                      }})}></input>
+                          <input maxLength="19" type="number" disabled={costToCredit == 0 ? true : false} id="rcnumber" {...register3("rcnumber",{minLength: {
+                                value: 19,
+                                message: "Card number must be of 19 digits",
+                            }})}></input>
                       
                         </div>
                         <div style={{width : "25%"}}>
                           <label htmlFor="rcnumber">PIN Number</label>
-                          <input ref={rcInputRef} id="rcpin" {...register2("rcpin",{minLength: {
-                        value: 4,
-                        message: "PIN must be of 4 digits",
-                      }})}></input>
+                          <input maxLength="4" type="number" disabled={costToCredit == 0 ? true : false} id="rcpin" {...register3("rcpin",{minLength: {
+                                value: 4,
+                                message: "PIN must be of 4 digits",
+                            }})}></input>
                         </div>
                       </div>
                       <div className="rewardButtons">
-                        <button disabled={loadingSubmit} style={{display: "flex", justifyContent: 'center', alignItems: 'center'}} type="submit" value="Apply">{loadingSubmit && <span className="spinner"></span>}APPLY</button>
+                        <button disabled={loadingSubmit || costToCredit == 0} style={{display: "flex", justifyContent: 'center', alignItems: 'center'}} type="submit" value="Apply">{loadingSubmit && <span className="spinner"></span>}APPLY</button>
                         <button type="button" onClick={()=> toggleModal()}>CHECK BALANCE</button>
                       </div>
-                      <span>$5.99 successfully applied to order. Your card will have $494.01 balance remaining after this transaction is completed.</span>
-                      
+
+                      {/* Error Handling */}
+                      {appliedMsg && !rcCard.errorMsg ? 
+                      <span>${rcDetails.length > 0 && rcDetails[rcDetails.length -1].amountPaidByRC} successfully applied to order. Your card will have ${rcDetails.length > 0 && (rcCard.balance - rcDetails[rcDetails.length -1].amountPaidByRC).toFixed(2)} balance remaining after this transaction is completed.</span>
+                      :
+                      <span>{rcCard.errorMsg}</span>}
+                      <ErrorMessage
+                        errors={errors3}
+                        name="rcnumber"
+                        render={({ messages }) => {
+                        console.log("messages", messages);
+                        return messages
+                            ? Object.entries(messages).map(([type, message]) => (
+                                <p key={type}>{message}</p>
+                            ))
+                            : null;
+                        }}
+                    />
+                      <ErrorMessage
+                        errors={errors3}
+                        name="rcpin"
+                        render={({ messages }) => {
+                        console.log("messages", messages);
+                        return messages
+                            ? Object.entries(messages).map(([type, message]) => (
+                                <p key={type}>{message}</p>
+                            ))
+                            : null;
+                        }}
+                    />
                     </form>
                 </div>
               </div>
